075fc5c9cb0bffe14f16e74aaa53163880ed61aa
Engr. Syed Rowshan Ali <engr.rowshan@gmail.com>
2018-09-25 16:13:23 +0600

    Init

--- Auto Cutter/Form1.Designer.cs
+++ Auto Cutter/Form1.Designer.cs
@@ -28,13 +28,66 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btn = new System.Windows.Forms.Button();
+            this.pic = new System.Windows.Forms.PictureBox();
+            this.nUP = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.pic)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nUP)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn
+            // 
+            this.btn.Location = new System.Drawing.Point(551, 22);
+            this.btn.Name = "btn";
+            this.btn.Size = new System.Drawing.Size(75, 23);
+            this.btn.TabIndex = 0;
+            this.btn.Text = "button1";
+            this.btn.UseVisualStyleBackColor = true;
+            this.btn.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // pic
+            // 
+            this.pic.Location = new System.Drawing.Point(-1, 1);
+            this.pic.Name = "pic";
+            this.pic.Size = new System.Drawing.Size(305, 457);
+            this.pic.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pic.TabIndex = 1;
+            this.pic.TabStop = false;
+            // 
+            // nUP
+            // 
+            this.nUP.Location = new System.Drawing.Point(551, 69);
+            this.nUP.Maximum = new decimal(new int[] {
+            254,
+            0,
+            0,
+            0});
+            this.nUP.Name = "nUP";
+            this.nUP.Size = new System.Drawing.Size(75, 20);
+            this.nUP.TabIndex = 2;
+            this.nUP.ValueChanged += new System.EventHandler(this.nUP_ValueChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(742, 457);
+            this.Controls.Add(this.nUP);
+            this.Controls.Add(this.pic);
+            this.Controls.Add(this.btn);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pic)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nUP)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btn;
+        private System.Windows.Forms.PictureBox pic;
+        private System.Windows.Forms.NumericUpDown nUP;
     }
 }
 
--- Auto Cutter/Form1.cs
+++ Auto Cutter/Form1.cs
@@ -12,9 +12,125 @@ namespace Auto_Cutter
 {
     public partial class Form1 : Form
     {
+        Bitmap b; 
+
         public Form1()
         {
             InitializeComponent();
+        }
+        List<int> gap;
+
+
+        private void btn_Click(object sender, EventArgs e)
+        {
+            b = new Bitmap(@"C:\Users\Rowshan\Desktop\Test\image2.png");
+            pic.Image = b;
+            System.GC.Collect();
+
+        }
+
+        public void check(int T = 0)
+        {
+            gap = new List<int>();
+
+            Color White = Color.White;
+            Color Red = Color.Red;
+
+            int R = 255;
+            int G = 255;
+            int B = 255;
+            
+            int RL = R - T;
+            int GL = R - T;
+            int BL = R - T;
+
+            // Draw line to screen.
+
+            for (int y = 0; y < b.Height; y++)
+            {
+                bool foundWhiteLine = true;
+                for (int x = 0; x < b.Width; x++)
+                {
+
+                    Color pixel = b.GetPixel(x, y);
+
+                    int TR = pixel.R;
+                    int TG = pixel.G;
+                    int TB = pixel.B;
+
+                    bool BR = false;
+                    bool BG = false;
+                    bool BB = false;
+
+                    if (TR <= R && TR >= RL) BR = true;
+                    if (TG <= G && TG >= GL) BG = true;
+                    if (TB <= B && TB >= BL) BB = true;
+
+                    if (BR && BG && BB)
+                    {
+                        //b.SetPixel(x, y, Red);
+                    }
+                    else
+                    {
+                        foundWhiteLine = false;
+                        break;
+                    }
+
+                }
+
+                if (foundWhiteLine)
+                {
+                    gap.Add(y);
+                    //for (int z = 0; z < b.Width; z++)
+                    //{
+                    //    b.SetPixel(z, y, Red);
+                    //}
+                }
+
+            }
+            fill();
+          pic.Refresh();
+
+
+        }
+
+
+        private void fill()
+        {
+            int f;
+            int s;
+            int max = 0;
+            int tm = 0;
+            int index = 0;
+
+
+            for(int x = 0; x < gap.Count-1; x++)
+            {
+                f = gap[x];
+                s = gap[x+1];
+                tm = s - f;
+               if(tm > max)
+                {
+                    max = tm;
+                    index = x;
+                }
+            }
+
+            index = gap[index];
+
+            for (int q = index; q < index + max; q++)
+            {
+                for (int p = 0; p < b.Width; p++)
+                {
+                    b.SetPixel(p, q, Color.Red);
+                }
+            }
+
+        }
+
+        private void nUP_ValueChanged(object sender, EventArgs e)
+        {
+            check((int)nUP.Value);
         }
     }
 }
